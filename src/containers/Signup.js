efc857c347244a3a66f4307057c4f0a3591c1b5b
msenior526 <msenior526@gmail.com>
2021-10-21 23:26:18 -0400

    Edit input names and add onChange event

--- src/containers/Signup.js
+++ src/containers/Signup.js
@@ -8,20 +8,37 @@ class Signup extends Component {
         location: ''
     }
 
+    handleSubmit = e => {
+        e.preventDefault();
+        console.log(this.state);
+        this.setState({
+            name: '',
+            username: '',
+            email: '',
+            location: ''
+        })
+    }
+
     render() {
         return (
             <div>
-                <form>
+                <form onSubmit={this.handleSubmit}>
                     <label for='name'>NAME</label>
-                    <input type='text' value={this.state.name}></input>
+                    <input type='text' name='name' value={this.state.name} onChange={this.handleChange}></input>
+
                     <label for='email'>EMAIL</label>
-                    <input type='email' value={this.state.email}></input>
+                    <input type='email' name='email' value={this.state.email} onChange={this.handleChange}></input>
+                    
                     <label for='username'>USERNAME</label>
-                    <input type='text' value={this.state.username}></input>
+                    <input type='text' name='username' value={this.state.username} onChange={this.handleChange}></input>
+                    
                     <label for='location'>LOCATION</label>
-                    <input type='text' value={this.state.location}></input>
+                    <input type='text' name='location' value={this.state.location} onChange={this.handleChange}></input>
+                    
                     <label for='password'>PASSWORD</label>
-                    <input type='password'></input>
+                    <input type='password' name='password' onChange={this.handleChange}></input>
+
+                    <input type='submit'></input>
                 </form>
             </div>
         )
