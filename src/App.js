44f7e968d1a564bf1a80de25de4291b772e31a64
msenior526 <msenior526@gmail.com>
2021-10-21 22:52:50 -0400

    import and create pet file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Pets from './containers/Pets'
 import React, { Component } from 'react';
 
 class App extends Component {
   render() {
     return (
     <div className="App">
+      <Pets />
     </div>
   );
   }
