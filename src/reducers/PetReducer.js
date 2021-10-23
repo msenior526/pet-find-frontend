ffac16487da8e44602821b27e13a0c4101031048
msenior526 <msenior526@gmail.com>
2021-10-23 18:52:47 -0400

    Add loading key to state, and add fetch pets action in reducer

--- src/reducers/PetReducer.js
+++ src/reducers/PetReducer.js
@@ -1,7 +1,11 @@
 export function PetReducer(state = {
-    pets: []
+    pets: [],
+    loading: false
 }, action) {
     switch(action.type) {
+
+        case 'FETCH_PETS': 
+            return {...state, loading: true}
         case 'ADD_PET':
             return {...state, pets: [...state.pets, action.pet]};
         default:
