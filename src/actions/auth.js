d50dbe4ee6dfd61eb5de0c8a0ac13186a1a5f937
msenior526 <msenior526@gmail.com>
2021-10-23 15:27:32 -0400

    Create action for user signup

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -2,3 +2,30 @@ export const setToken = (token) => {
     localStorage.setItem('token', token);
     localStorage.setItem("lastLoginTime", new Date(Date.now()).getTime());
 }
+
+export const signupUser = (userCredentials) => {
+    return dispatch => {
+        return fetch("http://localhost:3000/api/v1/users", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                Accept: 'application/json'
+            },
+            body: JSON.stringify({user: userCredentials})
+        })
+        .then(resp => {
+            debugger
+            if (resp.ok) {
+                setToken(resp.headers.get("Authorization"));
+                return resp.json().then((userJson) =>
+                        dispatch({ type: 'AUTHENTICATED', payload: userJson })
+                    );
+            } else {
+                return resp.json().then((errors) => {
+                    dispatch({ type: 'NOT_AUTHENTICATED' });
+                    return Promise.reject(errors);
+                });
+            }
+        })
+    };
+}
