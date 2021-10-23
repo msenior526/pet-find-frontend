05af595fe5a5feb6232a97a721f8b4bc4c238fcf
msenior526 <msenior526@gmail.com>
2021-10-23 17:16:22 -0400

    Add and create event handler for onChange

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -5,13 +5,26 @@ class Login extends Component {
         username: ''
     }
 
+    handleChange = e => {
+    
+    }
+
     render() {
         return (
             <form>
                 <label for='username'>USERNAME</label>
-                <input type='text' name='username' value={this.state.username}></input>
+                <input 
+                type='text' 
+                name='username' 
+                value={this.state.username}
+                onChange={this.handleChange}
+                ></input>
                 <label for='password'>PASSWORD</label>
-                <input type='password' name='password' value={this.state.password}></input>
+                <input type='password' 
+                name='password' 
+                value={this.state.password}
+                onChange={this.handleChange}
+                ></input>
             </form>
         )
     }
