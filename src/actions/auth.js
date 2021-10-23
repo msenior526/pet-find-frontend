b2f6e717492cfe8d3401957eea943f2bd3761539
msenior526 <msenior526@gmail.com>
2021-10-23 18:02:14 -0400

    Build action creator for user login

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -7,6 +7,34 @@ export const getToken = () => {
     localStorage.getItem("token")
 }
 
+export const loginUser = (userCredentials) => {
+    return dispatch => {
+        return fetch("http://localhost:3000/login", {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ user: userCredentials })
+        })
+        .then((resp) => {
+            if (resp.ok) {
+                setToken(resp.headers.get("Authorization"));
+                return resp
+                    .json()
+                    .then((userJson) =>
+                        dispatch({ type: 'AUTHENTICATED', payload: userJson })
+                    );
+            } else {
+                return resp.json().then((errors) => {
+                    dispatch({ type: 'NOT_AUTHENTICATED' });
+                    return Promise.reject(errors);
+                });
+            }
+        });
+    }
+}
+
 export const signupUser = (userCredentials) => {
     return dispatch => {
         return fetch("http://localhost:3000/api/v1/users", {
