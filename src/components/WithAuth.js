9e865fabeb9f1db866fc7af47ca9df6358dc1f87
msenior526 <msenior526@gmail.com>
2021-10-26 15:18:44 -0400

    Render jsx based on props

--- src/components/WithAuth.js
+++ src/components/WithAuth.js
@@ -1,30 +1,38 @@
 import React from "react";
 import { connect } from "react-redux";
-import { checkAuth } from "../actions/index";
+import { checkAuth } from "../actions/auth";
+import Login from "../containers/Login";
 
 function withAuth(WrappedComponent) {
-  class Wrapper extends React.Component {
-
-    render() {
-      return <div></div>
+    class Wrapper extends React.Component {
+        render() {
+            if (!this.props.authChecked) {
+                return <h3>LOADING</h3>;
+            } else if (!this.props.loggedIn) {
+                return (
+                    <Login />
+                );
+            } else {
+                return <WrappedComponent {...this.props} />;
+            }
+        }
     }
-  }
-
-  const mapStateToProps = state => {
-    return {
-        loggedIn: state.authReducer.loggedIn,
-        authChecked: state.authReducer.authChecked,
-        currentUser: state.authReducer.currentUser
+    
+    const mapStateToProps = state => {
+        return {
+            loggedIn: state.authReducer.loggedIn,
+            authChecked: state.authReducer.authChecked,
+            currentUser: state.authReducer.currentUser
+        };
     };
-  };
-
-  const mapDispatchToProps = dispatch => {
-    return {
-      checkAuth: () => dispatch(checkAuth())
+    
+    const mapDispatchToProps = dispatch => {
+        return {
+            checkAuth: () => dispatch(checkAuth())
+        };
     };
-  };
 
-  return connect(mapStateToProps, mapDispatchToProps)(Wrapper);
+    return connect(mapStateToProps, mapDispatchToProps)(Wrapper);
 }
 
 export default withAuth;
