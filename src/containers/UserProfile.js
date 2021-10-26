8463c4021b55c1b1a42822a926ec9e894856b589
msenior526 <msenior526@gmail.com>
2021-10-26 10:39:59 -0400

    Edit jsx in user profile

--- src/containers/UserProfile.js
+++ src/containers/UserProfile.js
@@ -2,7 +2,10 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 const UserProfile = props => (
-    <div>{props.currentUser.name}</div>
+    <div>
+        <h2>{props.currentUser.name}</h2>
+        <h4>Click to add new pet</h4>
+    </div>
 )
 
 const mapStateToProps = (state) => {
