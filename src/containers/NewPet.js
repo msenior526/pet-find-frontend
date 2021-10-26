e4e95a9c25b8aa45c16c6e251cb0839fa84a8223
msenior526 <msenior526@gmail.com>
2021-10-26 10:42:45 -0400

    Import and connect NewPet to redux store

--- src/containers/NewPet.js
+++ src/containers/NewPet.js
@@ -1,4 +1,5 @@
 import React, {Component} from "react";
+import {connect} from 'react-redux';
 
 class NewPet extends Component {
     render() {
@@ -6,4 +7,4 @@ class NewPet extends Component {
     }
 }
 
-export default NewPet;
+export default connect(NewPet);
