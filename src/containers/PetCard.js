cd0f0efd78cadf3b71227aab4f7fcaa3c9773012
msenior526 <msenior526@gmail.com>
2021-11-06 09:56:10 -0400

    Create function to handle favoriting action and add dispatch methods to handle favorites

--- src/containers/PetCard.js
+++ src/containers/PetCard.js
@@ -15,6 +15,15 @@ class PetCard extends Component {
     handleDelete = e => {
         this.props.deletePet(this.props.id)
     }
+    
+    handleFavorite = e => {
+        debugger
+        if (e.target.textContent === 'remove from favorites') {
+            return this.props.deleteFavorite()
+        } else if (e.target.textContent === 'add to favorites') {
+            return this.props.addFavorite()
+        }
+    }
 
     favoriteButtonText = () => {
         if (this.props.loggedIn) {
@@ -35,14 +44,16 @@ class PetCard extends Component {
                 <p>{this.props.age}</p>
                 <p>{this.props.species}</p>
                 <p>{this.props.gender}</p>
-                <button>{this.favoriteButtonText()}</button>
+                <button onClick={this.handleFavorite}>{this.favoriteButtonText()}</button>
                 {this.confirmUser() ? <button onClick={this.handleDelete}>delete pet</button> : null}
             </li>)}
 }
 
 const mapDispatchToProps = dispatch => {
     return {
-        deletePet: pet => dispatch(deletePet(pet)) 
+        deletePet: pet => dispatch(deletePet(pet)),
+        deleteFavorite: pet => dispatch(deleteFavorite(pet)),
+        addFavorite: pet => dispatch(addFavorite(pet))
     }
 }
 
