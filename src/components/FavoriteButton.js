28422e20a12038d758a5dce686359e4828099ffd
msenior526 <msenior526@gmail.com>
2021-11-06 11:58:57 -0400

    Create favorite buton class

---
+++ src/components/FavoriteButton.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class FavoriteButton extends Component {
+    state = {
+        text: 'Add to Favs'
+    }
+    
+    render() {
+        return <button>ADD TO FAVORITES</button>
+    }
+}
+
+export default FavoriteButton;
